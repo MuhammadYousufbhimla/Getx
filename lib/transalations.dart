3c53cc93afd7050e25b473903a87792e1571e10f
Muhammad Yousuf <Muhammadyousufgemini@gmail.com>
2022-09-20 14:31:38 +0500

    Language update

---
+++ lib/transalations.dart
@@ -0,0 +1,18 @@
+import 'package:get/get.dart';
+
+class Apptransaltion extends Translations{
+  @override
+  // TODO: implement keys
+  Map<String, Map<String, String>> get keys => {
+   "en_US":{
+
+    "sent1":"Welcome to My First getx Project.\n My name is Muhammad Yousuf\n Thanks...",
+    "title":"Demo Home Page",
+   },
+   "ur_Pk":{
+    "sent1":" میرے پہلے getx پروجیکٹ میں خوش آمدید۔ میرا نام محمد یوسف ہے شکریہ...",
+     "title":"getx ڈیمو ہوم پیج"
+   }
+  };
+
+}
