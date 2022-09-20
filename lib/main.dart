8c235da83493548eafb4bc05a43ac6e9479f894a
Muhammad Yousuf <Muhammadyousufgemini@gmail.com>
2022-09-20 16:15:32 +0500

    Getx languages dropdown

--- lib/main.dart
+++ lib/main.dart
@@ -43,12 +43,24 @@ class Homepage extends StatefulWidget {
 
 class _MyWidgetState extends State<Homepage> {
   @override
+String _dropDownValue = "";
+   String dropdownvalue = 'Item 1';  
+  int _value = 1;
+ var items = [   
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+  ];
+
    void _changelanguage(){
-
-
 Get.updateLocale(const Locale("ur","Pk"));
 
 
+   }
+     void _changelanguage1(){
+Get.updateLocale(const Locale("en","US"));
    }
   Widget build(BuildContext context) {
 
@@ -58,20 +70,82 @@ Get.updateLocale(const Locale("ur","Pk"));
     body: Center(
       child: Column(
      mainAxisAlignment: MainAxisAlignment.center,
-        // crossAxisAlignment: CrossAxisAlignment.center,
+      
         children: [
           Text('sent1'.tr,
           style: TextStyle(fontSize: 20),
-          ),
+
           
+          ),
+
+      
+
+               
+              Container(
+                 height: MediaQuery.of(context).size.height * 0.07,
+                 margin: EdgeInsets.all(20),
+                        padding: EdgeInsets.all(05),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color: Color.fromARGB(255, 2, 107, 6)),
+                            borderRadius: BorderRadius.circular(15)),
+                        child: DropdownButton<String>(
+                          isExpanded: true,
+                          underline: Container(),
+                          hint: _dropDownValue == ""
+                              ? Text('Select Language')
+                              : Text(
+                                  _dropDownValue,
+                                ),
+                          items: <String>[
+                            'English',
+                            'Urdu',
+                            'Arabic',
+                          ].map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(
+                                value,
+                                style: TextStyle(
+                                    color: Color.fromARGB(255, 2, 107, 6)),
+                              ),
+                            );
+                          }).toList(),
+                          onChanged: (value) {
+                            setState(() {
+                              
+                              _dropDownValue = value!;
+                               if(_dropDownValue=='English'){
+                                _changelanguage1();
+                               }
+                               else if(_dropDownValue=='Urdu'){
+                                _changelanguage();
+                               }
+                              print(_dropDownValue);
+                            });
+
+                       
+                           },
+                          
+                         ),
+              ), 
+
+
+      
+          // ElevatedButton(onPressed: (){
+          // _changelanguage1();
+          // }, child: Text("English"))
         ],
       ),
     ),
-     floatingActionButton: FloatingActionButton(
-        onPressed: _changelanguage,
-        tooltip: 'Increment',
-        child: const Icon(Icons.change_circle),
-     )  
+    //  floatingActionButton: FloatingActionButton(
+    //     onPressed: _changelanguage,
+        
+    //     tooltip: 'Increment',
+    //     child: const Icon(Icons.change_circle),
+    //  )  
+    //  ,
+     
     );
     
   }
