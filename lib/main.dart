7ee277e4a921ef639f1c235cc8665c29d893d2b5
Muhammad Yousuf <Muhammadyousufgemini@gmail.com>
2023-01-30 15:29:55 +0500

    GetX package used

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx/transalations.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -17,23 +18,17 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-
     return GetMaterialApp(
-      debugShowCheckedModeBanner:false,
+      debugShowCheckedModeBanner: false,
       translations: Apptransaltion(),
-      locale: const Locale('en','US'),
+      locale: const Locale('en', 'US'),
       title: 'Getx Demo',
       color: Colors.lightBlue,
-      home:Homepage() ,
-    
-    
-    
+      home: Homepage(),
     );
-    
-    
   }
-  
 }
+
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
 
@@ -43,10 +38,10 @@ class Homepage extends StatefulWidget {
 
 class _MyWidgetState extends State<Homepage> {
   @override
-String _dropDownValue = "";
-   String dropdownvalue = 'Item 1';  
+  String _dropDownValue = "";
+  String dropdownvalue = 'Item 1';
   int _value = 1;
- var items = [   
+  var items = [
     'Item 1',
     'Item 2',
     'Item 3',
@@ -54,104 +49,73 @@ String _dropDownValue = "";
     'Item 5',
   ];
 
-   void _changelanguage(){
-Get.updateLocale(const Locale("ur","Pk"));
+  void _changelanguage() {
+    Get.updateLocale(const Locale("ur", "Pk"));
+  }
 
+  void _changelanguage1() {
+    Get.updateLocale(const Locale("en", "US"));
+  }
 
-   }
-     void _changelanguage1(){
-Get.updateLocale(const Locale("en","US"));
-   }
   Widget build(BuildContext context) {
-
     return Scaffold(
-
-      appBar: AppBar(title:Text('title'.tr),),
-    body: Center(
-      child: Column(
-     mainAxisAlignment: MainAxisAlignment.center,
-      
-        children: [
-          Text('sent1'.tr,
-          style: TextStyle(fontSize: 20),
-
-          
-          ),
-
-      
-
-               
-              Container(
-                 height: MediaQuery.of(context).size.height * 0.07,
-                 margin: EdgeInsets.all(20),
-                        padding: EdgeInsets.all(05),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                                color: Color.fromARGB(255, 2, 107, 6)),
-                            borderRadius: BorderRadius.circular(15)),
-                        child: DropdownButton<String>(
-                          isExpanded: true,
-                          underline: Container(),
-                          hint: _dropDownValue == ""
-                              ? Text('Select Language')
-                              : Text(
-                                  _dropDownValue,
-                                ),
-                          items: <String>[
-                            'English',
-                            'Urdu',
-                            'Arabic',
-                          ].map((String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(
-                                value,
-                                style: TextStyle(
-                                    color: Color.fromARGB(255, 2, 107, 6)),
-                              ),
-                            );
-                          }).toList(),
-                          onChanged: (value) {
-                            setState(() {
-                              
-                              _dropDownValue = value!;
-                               if(_dropDownValue=='English'){
-                                _changelanguage1();
-                               }
-                               else if(_dropDownValue=='Urdu'){
-                                _changelanguage();
-                               }
-                               else if(_dropDownValue=='Arabic'){
-                                Get.updateLocale(const Locale("ar","Ar"));
-                                
-                               }
-                              print(_dropDownValue);
-                            });
-
-                       
-                           },
-                          
-                         ),
-              ), 
-
-
-      
-          // ElevatedButton(onPressed: (){
-          // _changelanguage1();
-          // }, child: Text("English"))
-        ],
+      appBar: AppBar(
+        title: Text('title'.tr),
       ),
-    ),
-    //  floatingActionButton: FloatingActionButton(
-    //     onPressed: _changelanguage,
-        
-    //     tooltip: 'Increment',
-    //     child: const Icon(Icons.change_circle),
-    //  )  
-    //  ,
-     
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'sent1'.tr,
+              style: TextStyle(fontSize: 20),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.07,
+              margin: EdgeInsets.all(20),
+              padding: EdgeInsets.all(05),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Color.fromARGB(255, 2, 107, 6)),
+                  borderRadius: BorderRadius.circular(15)),
+              child: DropdownButton<String>(
+                isExpanded: true,
+                underline: Container(),
+                hint: _dropDownValue == ""
+                    ? Text('Select Language')
+                    : Text(
+                        _dropDownValue,
+                      ),
+                items: <String>[
+                  'English',
+                  'Urdu',
+                  'Arabic',
+                ].map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(
+                      value,
+                      style: TextStyle(color: Color.fromARGB(255, 2, 107, 6)),
+                    ),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    _dropDownValue = value!;
+                    if (_dropDownValue == 'English') {
+                      _changelanguage1();
+                    } else if (_dropDownValue == 'Urdu') {
+                      _changelanguage();
+                    } else if (_dropDownValue == 'Arabic') {
+                      Get.updateLocale(const Locale("ar", "Ar"));
+                    }
+                    print(_dropDownValue);
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
-    
   }
 }
-
--- lib/transalations.dart
+++ lib/transalations.dart
@@ -5,14 +5,15 @@ class Apptransaltion extends Translations{
   // TODO: implement keys
   Map<String, Map<String, String>> get keys => {
    "en_US":{
-
     "sent1":"Welcome to My First getx Project.\n My name is Muhammad Yousuf\n Thanks...",
     "title":"Demo Home Page",
    },
+   
    "ur_Pk":{
     "sent1":" میرے پہلے getx پروجیکٹ میں خوش آمدید۔ میرا نام محمد یوسف ہے شکریہ...",
      "title":"getx ڈیمو ہوم پیج"
    },
+   
    "ar_Ar":{
     "sent1":" مرحبًا بك في أول مشروع getx الخاص بي. انا اسمي محمد يوسف شكرا ...",
      "title":"الصفحة الرئيسية التجريبية"
